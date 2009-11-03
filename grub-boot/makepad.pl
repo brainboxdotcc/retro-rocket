563dde3b3988433155d04134a77e23d068fb4406
Craig Edwards <craigedwards@brainbox.cc>
2009-11-03 19:19:22 +0000

    Pad disk images to 1.44mb. Hopefully not too far away we can do away with floppies (seriously, FLOPPIES???) and boot from CD

--- grub-boot/makepad.pl
+++ grub-boot/makepad.pl
@@ -2,4 +2,8 @@ my $s1 = -s 'stage1';
 my $s2 = -s 'stage2';
 my $num = 102400 - $s1 - $s2;
 system("dd if=/dev/zero of=pad bs=1 count=$num");
-
+system("cat  stage1 stage2 pad kernel.bin > floppy.img");
+my $s3 = -s 'floppy.img';
+$num = 1474560 - $s3;
+system("dd if=/dev/zero of=pad bs=1 count=$num");
+system("cat pad >>floppy.img");
