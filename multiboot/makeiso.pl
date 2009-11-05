4f0bc83a1630f3a133c3c64f2cbd1c00397cf8b9
Craig Edwards <craigedwards@brainbox.cc>
2009-11-05 22:43:53 +0000

    Correct the mkisofs syntax

--- multiboot/makeiso.pl
+++ multiboot/makeiso.pl
@@ -9,5 +9,5 @@ system("cp iso9660_stage1_5 iso/boot/grub/");
 system("cp stage* iso/boot/grub/");
 system("cp menu.lst iso/boot/grub/");
 chdir("iso");
-system("mkisofs -RV \"SIXTY-FOUR\" -b boot/grub/iso9660_stage1_5 -o ../sixty-four.iso -no-emul-boot -boot-load-size 4 -boot-info-table .");
+system("mkisofs -R -V \"SIXTY-FOUR\" -b boot/grub/iso9660_stage1_5 -o ../sixty-four.iso -no-emul-boot -boot-load-size 4 -boot-info-table .");
 
