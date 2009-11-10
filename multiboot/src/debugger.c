e0dc96ce9c2b89ea337052405b0501f83c540e67
Craig Edwards <craigedwards@brainbox.cc>
2009-11-10 00:03:56 +0000

    Split BBC-style DUMP command out to a seperate debugger.c. We can build a more advanced framework here. When debugging filesystems, hex dumps of ram are indespensible!

---
+++ multiboot/src/debugger.c
@@ -0,0 +1,19 @@
+#include "../include/debugger.h"
+
+void DumpHex(unsigned char* address, u32int length)
+{
+	int index = 0;
+	for(; index < length; index += 16)
+	{
+		printf("%04x: ", index);
+		int hex = 0;
+		for (; hex < 16; ++hex)
+			printf("%02x ", address[index + hex]);
+		putstring(current_console, " | ");
+		for (hex = 0; hex < 16; ++hex)
+			put(current_console, (address[index + hex] < 32 || address[index + hex] > 126) ? '.' : address[index + hex]);
+
+		put(current_console, '\n');
+	}
+}
+
--- multiboot/src/iso9660.c
+++ multiboot/src/iso9660.c
@@ -47,23 +47,6 @@ int ParsePVD(iso9660* info, unsigned char* buffer)
 	return info->root != 0;
 }
 
-void DumpHex(unsigned char* address, u32int length)
-{
-	int index = 0;
-	for(; index < length; index += 16)
-	{
-		printf("%04x: ", index);
-		int hex = 0;
-		for (; hex < 16; ++hex)
-			printf("%02x ", address[index + hex]);
-		putstring(current_console, " | ");
-		for (hex = 0; hex < 16; ++hex)
-			put(current_console, (address[index + hex] < 32 || address[index + hex] > 126) ? '.' : address[index + hex]);
-
-		put(current_console, '\n');
-	}
-}
-
 FS_DirectoryEntry* ParseDirectory(iso9660* info, u32int start_lba, u32int lengthbytes)
 {
 	unsigned char* dirbuffer = (unsigned char*)kmalloc(lengthbytes);
