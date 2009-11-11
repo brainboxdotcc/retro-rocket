b7637797f2b7385536e7f46d4575e1d33367f29f
Craig Edwards <craigedwards@brainbox.cc>
2009-11-11 14:37:35 +0000

    Comment

--- multiboot/src/timer.c
+++ multiboot/src/timer.c
@@ -22,6 +22,8 @@ void beep(u32int pitch)
 	if (tmp != (tmp | 3))
 		outb(0x61, tmp | 3);
 
+	// A beep is stopped by the timer interrupt half a second
+	// after it has been started, asyncronously and automatically.
 	beep_end = ticks + timer_freq / 2;
 }
  
@@ -50,23 +52,11 @@ static void timer_callback(registers_t regs)
 void init_timer(u32int frequency)
 {
 	timer_freq = frequency;
-
-	// Firstly, register our timer callback.
 	register_interrupt_handler(IRQ0, &timer_callback);
-
-	// The value we send to the PIT is the value to divide it's input clock
-	// (1193180 Hz) by, to get our required frequency. Important to note is
-	// that the divisor must be small enough to fit into 16-bits.
 	u32int divisor = 1193180 / frequency;
-
-	// Send the command byte.
 	outb(0x43, 0x36);
-
-	// Divisor has to be sent byte-wise, so split here into upper/lower bytes.
 	u8int l = (u8int)(divisor & 0xFF);
 	u8int h = (u8int)( (divisor>>8) & 0xFF );
-
-	// Send the frequency divisor.
 	outb(0x40, l);
 	outb(0x40, h);
 } 
