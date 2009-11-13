83994f247803c3dccbe921ad4745c298ba80a6de
Craig Edwards <craigedwards@brainbox.cc>
2009-11-13 02:01:05 +0000

    More makefile tidyups, filesystem tidyups, move grub stages and menu.lst into grub/ dir

--- multiboot/makeiso.pl
+++ multiboot/makeiso.pl
@@ -1,15 +1,16 @@
 #!/usr/bin/perl
 
-system("rm -rf iso");
-mkdir("iso");
-system("cp kernel.bin iso/");
-mkdir("iso/boot");
-mkdir("iso/boot/grub");
-system("cp iso9660_stage1_5 iso/boot/grub/");
-system("cp stage* iso/boot/grub/");
-system("cp menu.lst iso/boot/grub/");
-system("nm -a kernel.bin | sort -d >iso/kernel.sym");
-chdir("iso");
+system("rm -rf .iso");
+mkdir(".iso");
+system("cp kernel.bin .iso/");
+mkdir(".iso/boot");
+mkdir(".iso/boot/grub");
+system("cp grub/iso9660_stage1_5 .iso/boot/grub/");
+system("cp grub/stage* .iso/boot/grub/");
+system("cp grub/menu.lst .iso/boot/grub/");
+system("nm -a kernel.bin | sort -d >.iso/kernel.sym");
+chdir(".iso");
 system("mkisofs -R -V \"SIXTY-FOUR\" -b boot/grub/iso9660_stage1_5 -o ../sixty-four.iso -no-emul-boot -boot-load-size 4 -boot-info-table .");
 chdir("..");
 system("bzip2 -c sixty-four.iso > sixty-four.iso.bz2");
+
