c591bed35aa94e2917816e2602bc74fc717d1b6d
Craig Edwards <craigedwards@brainbox.cc>
2009-11-28 22:38:59 +0000

    Empty framework to start work on a kernel which is 64 bit only with SMP support.

---
+++ kernel64/build/makeiso.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+system("rm -rf .iso");
+mkdir(".iso");
+system("cp kernel.bin .iso/");
+mkdir(".iso/boot");
+mkdir(".iso/boot/grub");
+mkdir(".iso/devices");
+system("cp grub/iso9660_stage1_5 .iso/boot/grub/");
+system("cp grub/stage* .iso/boot/grub/");
+system("cp grub/menu.lst .iso/boot/grub/");
+system("cp sh .iso/");
+system("nm -a kernel.bin | sort -d >.iso/kernel.sym");
+chdir(".iso");
+system("mkisofs -R -V \"SIXTY-FOUR\" -b boot/grub/iso9660_stage1_5 -o ../sixty-four.iso -no-emul-boot -boot-load-size 4 -boot-info-table .");
+chdir("..");
+system("bzip2 -c sixty-four.iso > sixty-four.iso.bz2");
+
