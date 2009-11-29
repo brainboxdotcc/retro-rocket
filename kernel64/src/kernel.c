9c417fac2dd9126763d6cacc464156ad8711a171
Craig Edwards <craigedwards@brainbox.cc>
2009-11-29 13:41:23 +0000

    Add checking of multiboot and CPUID. We don't want to try booting on machines or emulators that dont support 64 bit

--- kernel64/src/kernel.c
+++ kernel64/src/kernel.c
@@ -1,8 +1,10 @@
+#include "../include/kernel.h"
+
 #define VIDEO_MEMORY			0xB8000
 
 unsigned char* VideoRam = (unsigned char*)VIDEO_MEMORY;
 
-void kmain()
+void kmain(MultiBoot* mb)
 {
 	VideoRam[0] = 'C';
 	VideoRam[2] = 'E';
