41bd93473d66e8457d05ef2445168ceb11f8bf43
Craig Edwards <craigedwards@brainbox.cc>
2009-12-03 02:12:30 +0000

    Disable caching on the page containing the apic

--- kernel64/src/apic.c
+++ kernel64/src/apic.c
@@ -1,7 +1,7 @@
 #include "../include/kernel.h"
 #include "../include/cpuid.h"
 
-#define APIC_ADDRESS 0x1000
+#define APIC_ADDRESS 0x4000
 #define APIC_BASE_MSR 0x1B
 #define APIC_BASE_MSR_ENABLE 0x800
 
@@ -9,7 +9,7 @@
 #define APIC_ID 0x0020
 #define APIC_VERSION 0x0030
 
-extern volatile u32* APIC;
+volatile u32* APIC;
 
 void read_msr(u32 msr, u32 *lo, u32 *hi)
 {
@@ -30,9 +30,11 @@ u64 get_apic_base()
 
 void set_apic_base(u64 apicaddr)
 {
+	memset((void*)apicaddr, 0xff, 4096);
 	u64 a=(apicaddr&0xfffff000) | APIC_BASE_MSR_ENABLE;
 	u32 d=(apicaddr>>32) & 0x0f;
 	write_msr(APIC_BASE_MSR, a, d);
+	APIC = apicaddr;
 }
 
 int apic_enabled()
@@ -42,6 +44,17 @@ int apic_enabled()
 	return ((a & (1 << 11)) >> 11);
 }
 
+u32 apic_read(u64 reg)
+{
+	return *((volatile u32 *)(APIC + reg));
+}
+
+void apic_write(u64 reg, u32 value)
+{
+	*((volatile u32 *)(APIC + reg)) = value;
+}
+
+
 /* Detect local APIC via CPUID */
 int detect_apic()
 {
@@ -49,9 +62,9 @@ int detect_apic()
 	cpuid(CPUID_GETFEATURES, &ecx, &edx);
 	if (edx & CPUID_FEAT_EDX_APIC)
 	{
-		printf("Relocating APIC...\n");
 		set_apic_base(APIC_ADDRESS);
-		printf("Detected a%s local APIC, base: %016x\n", apic_enabled() ? "n enabled" : " disabled", get_apic_base());
+		printf("Detected a%s local APIC, base: %08x\n", apic_enabled() ? "n enabled" : " disabled", get_apic_base());
+		printf("Local APIC version: %d ID: %d\n", apic_read(APIC_VERSION) & 0xFF, ((apic_read(APIC_ID))>>24)&0xFF);
 		return 1;
 	}
 	return 0;
