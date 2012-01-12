f5b178af6f8cba3d2ece9161c1dcfa065d920e7a
Craig Edwards <craigedwards@brainbox.cc>
2012-01-12 14:28:54 +0000

    Update copyright notices, dont want people ripping my interpreter atm.

--- multiboot/include/tokenizer.h
+++ multiboot/include/tokenizer.h
@@ -1,18 +1,7 @@
 /*
- * Copyright (c) 2006, Adam Dunkels
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the author nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * BBC BASIC interpreter,
+ * Retro Rocket OS Project (C) Craig Edwards 2012.
+ * loosely based on uBASIC (Copyright (c) 2006, Adam Dunkels, All rights reserved).
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
