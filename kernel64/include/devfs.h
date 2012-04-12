428f283db26d01462593d0a6af12290a0a690550
Craig Edwards <craigedwards@brainbox.cc>
2012-04-12 13:05:16 +0000

    Add devfs

---
+++ kernel64/include/devfs.h
@@ -0,0 +1,8 @@
+#ifndef __DEVFS_H__
+#define __DEVFS_H__
+
+#include "kernel.h"
+
+void init_devfs();
+
+#endif
---
+++ kernel64/src/devfs.c
@@ -0,0 +1,43 @@
+#include <devfs.h>
+#include <filesystem.h>
+#include <kmalloc.h>
+#include <string.h>
+#include <kprintf.h>
+
+FS_FileSystem* devfs = NULL;
+FS_DirectoryEntry* devfs_entries = NULL;
+
+void* devfs_get_directory(void* t)
+{
+	FS_Tree* treeitem = (FS_Tree*)t;
+	devfs_entries->directory = treeitem;
+	return devfs_entries;
+}
+
+int devfs_read_file(void* file, u32int start, u32int length, unsigned char* buffer)
+{
+	return 0;
+}
+
+void init_devfs()
+{
+	return;
+	devfs = (FS_FileSystem*)kmalloc(sizeof(FS_FileSystem));
+	strlcpy(devfs->name, "devfs", 31);
+	devfs->getdir = devfs_get_directory;
+	devfs->readfile = devfs_read_file;
+	devfs->writefile = NULL;
+	devfs->rm = NULL;
+	register_filesystem(devfs);
+	devfs_entries = (FS_DirectoryEntry*)kmalloc(sizeof(FS_DirectoryEntry));
+	devfs_entries->next = NULL;
+	devfs_entries->size = 0;
+	devfs_entries->flags = 0;
+	devfs_entries->filename = strdup("core");
+	//kprintf("Calling attach\n");
+	/* NB: The /devices mountpoint must exist in the root fs */
+	attach_filesystem("/devices", devfs, NULL);
+	//kprintf("attach done\n");
+}
+
+
