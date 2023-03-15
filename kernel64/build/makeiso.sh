7eb9b34a731545c4c4a1c93ed66eb530f6d1bbd3
Craig Edwards <craigedwards@brainbox.cc>
2023-03-15 08:15:53 +0000

    change makeiso to a shell script, doesnt do anything to require it to be perl

---
+++ kernel64/build/makeiso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf .iso
+mkdir .iso
+cp kernel.bin .iso/
+mkdir .iso/devices
+mkdir .iso/harddisk
+cp -v limine.cfg limine/limine.sys limine/limine-cd.bin limine/limine-cd-efi.bin .iso/
+nm -a kernel.bin | sort -d >.iso/kernel.sym
+rm -rf .iso/os
+cp -rv os/* .iso/
+xorriso -as mkisofs -b limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table -V "RETRO-ROCKET" --protective-msdos-label .iso -o sixty-four.iso
+bzip2 -c sixty-four.iso > sixty-four.iso.bz2
+
