47c6c50eba0e38fc7ca2641d4cefe3661658f360
Craig Edwards <craigedwards@brainbox.cc>
2023-04-02 13:00:00 +0000

    add kernel address request

--- include/limine-requests.h
+++ include/limine-requests.h
@@ -12,3 +12,8 @@ volatile struct limine_hhdm_request hhdm_request = {
     .revision = 0,
 };
 
+volatile struct limine_kernel_address_request address_request = {
+	.id = LIMINE_KERNEL_ADDRESS_REQUEST,
+	.revision = 0,
+};
+
