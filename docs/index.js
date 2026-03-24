478348f16757727a495af83fc8864ac610c2b530
Craig Edwards <craigedwards@brainbox.cc>
2026-03-24 23:35:51 +0000

    fix

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+var index =
+[
+    [ "User Guide", "user-guide.html", "user-guide" ],
+    [ "BASIC Language Reference", "basic-ref.html", "basic-ref" ],
+    [ "Kernel Development", "kernel-dev.html", "kernel-dev" ],
+    [ "Third Party Notices", "third-party-notices.html", null ]
+];
